fbd2ba9cdbf7a9ba4fd8a4db73a49109d823cc43
Donovan Stevenson <donovan.stevenson@wicourts.gov>
2022-09-27 10:32:18 -0500

    Restore array reference equality check   - This reverts the change in https://github.com/purescript/purescript-prelude/pull/187   - Provides a major optimization for array equality checking.   - Opting into this functionality within all nested records at the     moment seems unrealistic compared to just restoring this here.
    Reviewed By: pbrant

--- src/Data/Eq.js
+++ src/Data/Eq.js
@@ -13,6 +13,7 @@ export const eqStringImpl = refEq;
 export const eqArrayImpl = function (f) {
   return function (xs) {
     return function (ys) {
+      if (xs === ys) return true;
       if (xs.length !== ys.length) return false;
       for (var i = 0; i < xs.length; i++) {
         if (!f(xs[i])(ys[i])) return false;
